5f8518b02e92475fa4dd3b52310ae5827d01a664
robgmerrill <robgmerrill@gmail.com>
2017-05-30 20:43:10 -0700

    first commit

---
+++ js/MyApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div>
+    <h2 className="name">
+      My name is Rob
+    </h2>
+    <h1>This will work</h1>
+  </div>,
+  document.getElementById("app")
+);
